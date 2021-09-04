7f1b3ff37cf1ba123476d310f6a743f2d8beffa7
Licaodong-code <1776782142@qq.com>
2021-09-04 20:37:08 +0800

    Site updated: 2021-09-04 20:37:07

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://github.com/Licaodong-code/Blog-Back-Up/blob/main/min_photos/' + data.link[i];
-          var src = 'https://github.com/Licaodong-code/Blog-Back-Up/blob/main/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/Licaodong-code/Blog-Photo/master/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/Licaodong-code/Blog-Photo/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
